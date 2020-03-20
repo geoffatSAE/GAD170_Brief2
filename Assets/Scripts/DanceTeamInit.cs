aa7f634e48681b2db1f5ed612485b874aec295ed
Geoffrey Hil <g.hill@sae.edu>
2020-03-20 12:57:17 +1100

    Made dance floor cool.

--- Assets/Scripts/DanceTeamInit.cs
+++ Assets/Scripts/DanceTeamInit.cs
@@ -7,7 +7,7 @@ using System.Linq;
 /// This class generates and assigns names to the 2 dance teams in our dance off battle.
 /// It also controls the number of dancers on each team via the inspector
 /// It also uses the name generator to pass character names to the teams so they can initialise
-/// 
+/// bunch of cool code added here. wow do so much,.
 /// TODO:
 ///     Generate unique team names for both teams and assign them via team_.SetTroupeName(str);
 ///     Use the nameGenerator to get enough names for the number of dancers on both teams and pass the required names via array to each team for init (InitaliseTeamFromNames)
